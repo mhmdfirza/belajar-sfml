b13b13eec53e1a62ee9fd50bca5a62cbddfed650
mhmdfirza <muhammad.firzatullah.ar@gmail.com>
2026-07-22 08:25:36 +0700

    logic peluru

--- main.cpp
+++ main.cpp
@@ -37,51 +37,43 @@ int main()
     sf::Vector2f shipPosition = ship.getPosition();
 
     // LOAD PELURU
-    sf::CircleShape bullet(0.1f);
-    bullet.setFillColor(sf::Color::Yellow);
     struct Bullet{
         sf::CircleShape shape;
         sf::Vector2f velocity;
     };
     sf::FloatRect boundsBullet = bullet.getLocalBounds();
     bullet.setOrigin({boundsBullet.size.x/2.0f, boundsBullet.size.y/2.0f});
-    sf::Vector2f bulletPosition({
-                                shipPosition.x + 5.0f,
-                                shipPosition.y;
-                                });
+    sf::Vector2f bulletPosition({shipPosition.x + 5.0f, shipPosition.y});
+
+    std::vector<Bullet> bullets;
+
+    sf::Clock fireClock;
+    const float fireRate = 0.2f;
+
+    sf::Clock deltaClock;
 
     // LOAD TEXT
     sf::String displayString = "Mainan Tembak tembak";
     sf::Text text(font, displayString,24);
     auto boundsText = text.getLocalBounds();
-    text.setOrigin({
-        boundsText.position.x + boundsText.size.x / 2.f,
-        boundsText.position.y + boundsText.size.y / 2.f
-    });
+    text.setOrigin({boundsText.position.x + boundsText.size.x / 2.f, boundsText.position.y + boundsText.size.y / 2.f});
     text.setFillColor(sf::Color::White);
 
     // --------- SET OBJECT & TEXT KE TENGAH -------
-    ship.setPosition({
-                     static_cast<float>(windowSize.x)/2.f,
-                     static_cast<float>(windowSize.y)/2.f
-                     });
-    text.setPosition({
-                     static_cast<float>(windowSize.x)/2.f,
-                     // static_cast<float>(windowsSize.x)/2.f
-                     static_cast<float>(windowSize.y)/6.f}
-                     );
+    ship.setPosition({static_cast<float>(windowSize.x)/2.f, static_cast<float>(windowSize.y)/2.f});
+    text.setPosition({static_cast<float>(windowSize.x)/2.f, static_cast<float>(windowSize.y)/6.f});
+    // static_cast<float>(windowsSize.x)/2.f
 
-    // LOAD MUSIC
-    sf::Music music;
-    if (!music.openFromFile("resources/music/aud1.mp3")){
-        cout << "Error: Failed Load Music\n";
-        return -1;
-    }
-    music.play();
-    music.setVolume(50);
+    // LOAD MUSIC uncomment shift+ctrl+x
+//    sf::Music music;
+//    if (!music.openFromFile("resources/music/aud1.mp3")){
+//        cout << "Error: Failed Load Music\n";
+//        return -1;
+//    }
+//    music.play();
+//    music.setVolume(50);
 
     float speed = 1.f;
-    float offset = 20.f;
 
     // ----------- RENDER GAME MAIN -----------
     while(window.isOpen())
@@ -116,9 +108,40 @@ int main()
 //        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space))
 //            float.bullet(ship.x, ship.y - offset);
 
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space))
-            // spawn bullet dan tembak ke arah yang dilihat oleh object
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)&& fireClock.getElapsedTime().asSeconds()>=fireRate)
+            window.draw(bullet);// spawn bullet dan tembak ke arah yang dilihat oleh object
 
+            // sudut rotasi kapal dalam radian
+            float angleRad = ship.getRotation().asRadians();
+
+            // kecepatan peluru
+            float bulletSpeed = 500.f
+
+            // hitung arah vektor bergerak maju
+            sf::Vector2f bulletVelocity(
+                std::sin(angleRad) * speed,
+                -std::cos(angleRad) * speed
+                                        );
+            // moncong depan kapal
+            float offset = (boundsShip.size.y * ship.getScale().y)/ 2.0f;
+            sf::Vector2f spawnPos = {
+                ship.getPosition().x + offset * std::sin(angleRad),
+                ship.getPosition().y - offset * std::cos(angleRad)
+                };
+
+            Bullet newBullet;
+            newBullet.shape.setRadius(5.f);
+            newBullet.shape.setFillColor(sf::Color::Yellow);
+            newBullet.shape.setOrigin ({5.f,5.f});
+            newBullet.shape.setPosition(spawnPos);
+            newBullet.velocity = bulletVelocity;
+
+            // masuk magazine
+            bullets.push_back(newBullet);
+
+            // cooldown
+            fireClock.restart();
+        }
 
         window.clear();
 
